d8a708a3956c372c58beed48067cb59d00360232
tringuyen07 <changelle24@gmail.com>
2025-07-18 15:52:06 +0700

    template admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group([
+    'prefix' => 'admin',
+    'as' => 'admin',
+    'namespace' => 'App\Http\Controllers\Admin'
+], function () {
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
